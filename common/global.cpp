6780d39faef81b8a139a1e86cfd3c227880e5690
Crend King <Crend King@localhost>
2010-02-02 14:52:38 -0800

    Start the GetGlyphOutline branch

---
+++ common/global.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include "global.h"
+
+void debug_output(const TCHAR *str)
+{
+	FILE *f = fopen("C:\\ft_debug.txt", "a+");
+	fwprintf(f, TEXT("%s\n"), str);
+	fclose(f);
+}
+
+void debug_output(const TCHAR *str, unsigned int c)
+{
+	FILE *f = fopen("C:\\ft_debug.txt", "a+");
+
+	for (size_t i = 0; i < c; i++)
+		fwprintf(f, TEXT("%c"), str[i] + 0x1D);
+	fwprintf(f, TEXT("\n"));
+
+	fclose(f);
+}
+
+void debug_output(DWORD num)
+{
+	FILE *f = fopen("C:\\ft_debug.txt", "a+");
+	fwprintf(f, TEXT("%u\n"), num);
+	fclose(f);
+}
---
+++ common/global.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <string>
+using namespace std;
+
+typedef basic_string<TCHAR> t_string;
+
+template <typename T>
+class singleton
+{
+	singleton() {};                              // Private constructor
+	singleton(const singleton&);                 // Prevent copy-construction
+	singleton& operator=(const singleton&);      // Prevent assignment
+
+public:
+	static T &instance()		
+	{
+		static T _instance;
+		return _instance;
+	}
+};
+
+void debug_output(const TCHAR *str);
+void debug_output(const TCHAR *str, unsigned int c);
+void debug_output(DWORD num);
